a74e652fbbbad175ef3ac70118adbd1b80ba12bc
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2019-08-23 08:05:38 +0200

    Update ratzfatz_edit.ps1

--- exchange/ratzfatz_edit.ps1
+++ exchange/ratzfatz_edit.ps1
@@ -1,9 +1,19 @@
+# ---------------------------------------------
 # Author: Valentin Klopfenstein
 # Made in August 2019
-# Use it at will
+#
+# Use at will without credit
+# ---------------------------------------------
+
+# Set up inbound params
+param(
+	[string] $enableLog
+)
+
+# Constant variables
+$globalPath = Get-Location
 
 # Define main menu
-
 function Main-Menu {
     param (
         [string]$Title = 'Exchange Ratzfatz edit'
@@ -15,21 +25,30 @@ function Main-Menu {
     
     Write-Host "1: Assign Mailbox permissions"
     Write-Host "2: Export Mailbox"
-    Write-Host "3: Get all Memmbers of the OGAL"
+    Write-Host "3: Get all Memmbers of the global OAB"
     Write-Host "Q: Abort"
+	
+	if ($enableLog = "ToLog") {
+		Write-Host ''
+		Write-Host '[i] Logging is active!'
+		Write-Host "[i] Logs are saved in $globalPath"
+	}
 }
 
 # Assign Mailbox permission
 function assignPerm {
-        ''
-        '================================='
-        '> Assign Mailbox permissions'
-        ''
+        Write-Host ''
+        Write-Host '================================='
+        Write-Host '> Assign Mailbox permissions'
+        Write-Host ''
+		
         $ident = Read-Host "Define 'identity'"
         $user = Read-Host "Define 'user'"
-        ''
-        "[i] For reference on possible AccessRights params, consult the following:"
-        '[>] https://docs.microsoft.com/en-us/powershell/module/exchange/mailboxes/set-mailboxfolderpermission?view=exchange-ps#parameters'
+		
+        Write-Host ''
+		
+        Write-Host "[i] For reference on possible AccessRights params, consult the following:"
+        Write-Host '[>] https://docs.microsoft.com/en-us/powershell/module/exchange/mailboxes/set-mailboxfolderpermission?view=exchange-ps#parameters'
         $acr = Read-Host "Set Access level"
 
         Add-MailboxPermissions -Identity "$ident" -User "$user" -AccessRights "$acr"
@@ -42,9 +61,9 @@ function assignPerm {
 
 # Export Mailbox
 function exportMail {
-    ''
-    '[i] Caution! Only possible in on-premises configuration!'
-    ''
+    Write-Host ''
+    Write-Host '[i] Caution! Only possible in on-premises configuration!'
+    Write-Host ''
 
     $target = Read-Host "Target Mailbox"
     $path = Read-Host "Path (Must be UNC)"
@@ -58,6 +77,20 @@ function exportMail {
     main
 }
 
+# Acquire global offline address book
+function getGOAB {
+	Write-Host ''
+	
+	$a = get-addresslist "Offline Global Address List"
+	get-recipient -filter $a.recipientfilter
+	
+    Write-Host "Done! Press RETURN to proceed"
+    pause >NUL
+
+    main
+}
+
+# Actual program
 function main {
 cls
     do
@@ -79,9 +112,19 @@ cls
 
             } '3' {
               'NOT YET COMPLETE'
-            }
+
+            } default {
+
+                'Invalid input!'
+
+            } 'Q' {
+            Write-Host 'Good day!'
+            exit }
         }
-        pause
+        
+        Write-Host 'Press RETURN to proceed.'
+        pause >NUL
+
      }
      until ($selection -eq 'q')
  }
